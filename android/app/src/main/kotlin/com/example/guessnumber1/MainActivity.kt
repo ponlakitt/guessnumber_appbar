cea07c0935408fc77a250695951adf26d54371a9
Ponlakit Konkrathok <Ponlakit13@hotmail.com>
2022-01-01 23:44:48 +0700

    homework4

---
+++ android/app/src/main/kotlin/com/example/guessnumber1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.guessnumber1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/game.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: avoid_print
+
+import 'dart:math';
+import 'dart:io';
+
+class Game { // camel case
+  int? _answer ;
+  int _guessCount = 0;
+  int? max;
+  Game({int? maxRandom}){
+    maxRandom ??= 100;
+     max = maxRandom ;
+    _answer = Random().nextInt(max!) + 1 ;
+  }
+  int get guessCount {
+    return _guessCount;
+  }
+  int? get maxNum{
+    return max;
+  }
+  int doGuess(int num){
+    // ทายถูก return 0
+    // ทายมากไป return 1
+    // ทายน้อยไป return -1
+    _guessCount++;
+    if(num > _answer!){
+      return 1 ;
+    }else if (num < _answer!){
+      return -1 ;
+    }else {
+      return 0 ;
+    }
+  }
+}
