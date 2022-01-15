81175e2fc0077295cba8fc41fda21623e69e3541
Ponlakit Konkrathok <Ponlakit13@hotmail.com>
2022-01-15 23:42:36 +0700

    homework6

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,37 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Game {
+  static const defaultMaxRandom = 100;
+  int? _answer;
+  int _guessCount = 0;
+  static final List<int> guessCountList = [];
+
+  Game({int maxRandom = defaultMaxRandom}) {
+    var r = Random();
+    _answer = r.nextInt(maxRandom) + 1;
+  }
+
+  int get guessCount {
+    return _guessCount;
+  }
+
+  void addCountList() {
+    guessCountList.add(_guessCount);
+  }
+
+  int doGuess(int num) {
+    _guessCount++;
+    if (num > _answer!) {
+      return 1;
+    } else if (num < _answer!) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -10,61 +43,136 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GUESS THE NUMBER',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.purple,
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+  var game = Game();
+  var check = false;
+  final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('GUESS THE NUMBER')),
-      body: Container(
-        decoration: BoxDecoration(
-            color: Colors.orange.shade100,
-            border: Border.all(width: 5.0, color: Colors.orange.shade100),
+      appBar: AppBar(
+        title: const Text('GUESS THE NUMBER'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.purple.shade50,
+            borderRadius: BorderRadius.circular(16.0),
             boxShadow: [
               BoxShadow(
-                color: Colors.white.withOpacity(0.5),
-                offset: const Offset(2.0, 5.0),
-                blurRadius: 5.0,
+                color: Colors.purple.shade100,
+                offset: Offset(5.0, 5.0),
                 spreadRadius: 2.0,
-              ),
-            ]),
-        //alignment: Alignment.center,
-        child: Center(
+                blurRadius: 5.0,
+              )
+            ],
+          ),
+          alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/guess_logo.png', width: 100.0),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(' GUESS',style:TextStyle(fontSize: 40,color: Colors.orange.shade200),),
-                      Text('   THE NUMBER',style:TextStyle(fontSize: 20,color: Colors.orange.shade400),),
-                    ],)
-                ],
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/guess_logo.png', width: 90.0),
+                    SizedBox(width: 8.0),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('GUESS',
+                            style: TextStyle(
+                                fontSize: 36.0, color: Colors.purple.shade200)),
+                        Text(
+                          'THE NUMBER',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.purple.shade600,
+                            //fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-          SizedBox(height: 40,),
-          Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: TextField(),
-          ),
-              ElevatedButton(
-                child: Text('GUESS'),
-                onPressed: () {
-                  // โค้ดที่จะทำงานเมื่อกดปุ่ม
-                },
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.7),
+                    border: OutlineInputBorder(),
+                    hintText: 'Guess number between 1 and 100',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: ElevatedButton(
+                  child: Text('GUESS'),
+                  onPressed: () {
+                    if(check)
+                    {
+                      game = Game();
+                      check =false;
+                    }
+                    var input = _controller.text;
+                    var guess = int.tryParse(input!);
+                    var a = '';
+                    if (guess != null) {
+                      var result = game.doGuess(guess);
+                      if (result == 1) {
+                        a =' $guess is TOO HIGH!';
+                      } else if (result == -1) {
+                        a=' $guess is TOO LOW!';
+                      } else if (result == 0) {
+                        a= ' $guess is CORRECT, total guesses:'
+                            '${game.guessCount}';
+                        check = true;
+                        game.addCountList();
+                      }
+                    }
+                    else
+                    {
+                      a = 'Incorrect!!! , Input number 1 - 100 ';
+                    }
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('RESULT'),
+                          content: Text(a),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
             ],
           ),
